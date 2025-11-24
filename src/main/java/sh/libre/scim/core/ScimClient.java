51ce78767e9af3171124c865855cae7bc1b9337b
Harshad Patel <harshad.patel@dsv.daikin.com>
2025-11-24 12:48:47 -0800

    Add detailed logging for SCIM list requests to debug fetch failures

--- src/main/java/sh/libre/scim/core/ScimClient.java
+++ src/main/java/sh/libre/scim/core/ScimClient.java
@@ -126,19 +126,23 @@ public class ScimClient {
     private <S extends ResourceNode> List<S> fetchAllResources(String endpoint, Class<S> resourceClass) {
         List<S> allResources = new ArrayList<>();
         try {
+            String listUrl = "/" + endpoint;
+            LOGGER.debugf("Sending SCIM list request to URL: %s%s", scimApplicationBaseUrl, listUrl);
             ServerResponse<ListResponse<S>> pageResponse = scimRequestBuilder
-                .list("/" + endpoint, resourceClass)
+                .list(listUrl, resourceClass)
                 .get()
                 .sendRequest();
+            LOGGER.debug("Received response for list request: status=" + pageResponse.getHttpStatus() + ", success=" + pageResponse.isSuccess());
             if (pageResponse.isSuccess()) {
                 ListResponse<S> page = pageResponse.getResource();
                 allResources.addAll(page.getListedResources());
+                LOGGER.debugf("Fetched %d resources from response", allResources.size());
                 // Note: Assuming Databricks returns all resources in one page or we take the first page
             } else {
-                LOGGER.warnf("Failed to fetch resources: %d %s", pageResponse.getHttpStatus(), pageResponse.getResponseBody());
+                LOGGER.warnf("Failed to fetch resources: HTTP %d - %s", pageResponse.getHttpStatus(), pageResponse.getResponseBody());
             }
         } catch (Exception e) {
-            LOGGER.errorf("Failed to fetch resources: %s", e.getMessage());
+            LOGGER.errorf("Failed to fetch resources: %s", e.getMessage(), e);
         }
         return allResources;
     }
